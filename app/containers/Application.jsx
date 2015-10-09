030aca490c24ab689a89056fe1a47d8bf36d8327
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-10-09 00:55:30 -0700

    updates to react .14

--- app/containers/Application.jsx
+++ app/containers/Application.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { RouteHandler } from "react-router";
 
+import { stores } from "../stores"
+
 import bootstrap from "bootstrap-webpack"
 import fontawesome from "font-awesome-webpack"
 import styles from "./Application.less"
@@ -8,14 +10,19 @@ import styles from "./Application.less"
 import { Grid, Row, Col } from "react-bootstrap"
 
 export default class Application extends React.Component {
+  static getProps(params) {
+    return {}
+  }
+  /*
   static getProps(stores, params) {
     var transition = stores.Router.getItem("transition")
     return {
       loading: !!transition
     }
   }
+  */
   render() {
-    var { loading } = this.props;
+   // var { loading } = this.props;
         return <Grid>
             <Row>
                 <Col xs={12}>
@@ -62,7 +69,9 @@ export default class Application extends React.Component {
         document.getElementById("content")
       )
     })
-*/
+
+
 Application.contextTypes = {
   stores: React.PropTypes.object
 };
+*/
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Router, Route } from "react-router"
 import routes from "./routes"
 import { stores } from "./stores"
-import renderApplication from "./render"
 
-console.dir(routes)
+import Application  from "routes/Application"
+import Home from "routes/HomePage"
+import NoMatch from "routes/NotFoundPage"
 
+// Declarative route configuration (could also load this config lazily
+// instead, all you really need is a single root route, you don't need to
+// colocate the entire config).
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={Application}>
+      <Route path="home" component={Home}/>
+      <Route path="*" component={NoMatch}/>
+    </Route>
+  </Router>
+), document.getElementById("content"))
+
+/*import renderApplication from "./render"
 renderApplication(routes, stores, {
   timeout: 600
 })
+*/
